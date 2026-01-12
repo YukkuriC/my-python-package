30afe788563ee69df004edafea558bdce8b6a735
YukkuriC <799433638@qq.com>
2026-01-12 23:06:12 +0800

    renamed

--- YukkuriC/files/link.py
+++ YukkuriC/files/link.py
@@ -1,7 +1,7 @@
 import os, subprocess
 
 
-def linkFolder(link: str, src: str, flag='/J'):
+def link_folder(link: str, src: str, flag='/J'):
     if not os.path.isdir(src) or os.path.isdir(link):
         return
     os.makedirs(os.path.dirname(link), exist_ok=True)
@@ -9,3 +9,6 @@ def linkFolder(link: str, src: str, flag='/J'):
         ['cmd', '/c', 'mklink', flag, os.path.abspath(link), os.path.abspath(src)],
         shell=True,
     )
+
+
+__all__ = ['link_folder']
--- tools/linkSelf.py
+++ tools/linkSelf.py
@@ -3,12 +3,12 @@ import os, sys
 root_path = os.path.dirname(os.path.dirname(__file__))
 sys.path.insert(0, root_path)
 
-from YukkuriC.files.link import linkFolder
+from YukkuriC.files.link import *
 
 module_name = 'YukkuriC'
 modules_root = [p for p in sys.path if 'site-packages' in p][0]
 print('target =', modules_root)
-linkFolder(
+link_folder(
     os.path.join(modules_root, module_name),
     os.path.join(root_path, module_name),
 )
